b5a58d696d4ba3d5c4b62db16f5c988b881ba288
Momtchil Momtchev <momtchil@momtchev.com>
2022-09-28 21:13:11 +0200

    remove generated files from git

--- build/front/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-require("./App.css");
-function App() {
-    return ((0, jsx_runtime_1.jsx)("div", { className: "App", children: (0, jsx_runtime_1.jsx)("header", { className: "App-header" }) }));
-}
-exports.default = App;
