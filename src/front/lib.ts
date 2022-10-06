7f003604e89c21f67706608acaee24142d7875f4
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-06 15:23:39 +0200

    center the element into view

--- src/front/lib.ts
+++ src/front/lib.ts
@@ -1,9 +1,9 @@
 export function scrollIntoViewIfNeeded(target: HTMLElement) {
     if (target.getBoundingClientRect().bottom > window.innerHeight) {
-        target.scrollIntoView({block: 'end', inline: 'nearest', behavior: 'smooth'});
+        target.scrollIntoView({block: 'center', inline: 'nearest', behavior: 'smooth'});
     }
 
     if (target.getBoundingClientRect().top < 0) {
-        target.scrollIntoView({block: 'start', inline: 'nearest', behavior: 'smooth'});
+        target.scrollIntoView({block: 'center', inline: 'nearest', behavior: 'smooth'});
     }
 }
