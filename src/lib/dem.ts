28e1cf4798db9110b8cfb5b27b07d5084577f5e1
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-07 14:14:08 +0200

    use atomic operations when dealing with the shared cache

--- src/lib/dem.ts
+++ src/lib/dem.ts
@@ -6,31 +6,43 @@ import {Point} from './flight';
 
 import config from '../config.json';
 
-let lastSRTMFile = '';
-let lastSRTMds: gdal.Dataset = null;
-let lastSRTMxform: gdal.CoordinateTransformation = null;
-let lastSRTMData: NdArray = null;
+const last = {
+    file: '',
+    ds: null as gdal.Dataset,
+    xform: null as gdal.CoordinateTransformation,
+    data: null as NdArray
+};
 
 export async function terrainUnderPath(track: Point[]) {
     for (const p of track) {
+        let ds: gdal.Dataset;
+        let xform: gdal.CoordinateTransformation;
+        let data: NdArray;
+
         const file = `${p.lat > 0 ? 'N' : 'S'}${Math.floor(p.lat).toFixed(0)}${p.lng > 0 ? 'E' : 'W'}${Math.floor(p.lng)
             .toFixed(0)
             .padStart(3, '0')}.hgt`;
-        if (file !== lastSRTMFile) {
-            lastSRTMFile = file;
-            try {
-                lastSRTMds = await gdal.openAsync(path.resolve(config.dbserver.srtm_dir, lastSRTMFile));
-                lastSRTMxform = new gdal.CoordinateTransformation(gdal.SpatialReference.fromEPSG(4326), lastSRTMds);
-                lastSRTMData = await lastSRTMds.bands.get(1).pixels.readArrayAsync();
-            } catch (e) {
-                // eslint-disable-next-line no-console
-                console.error(e);
-                lastSRTMData = null;
+        try {
+            if (file !== last.file) {
+                ds = await gdal.openAsync(path.resolve(config.dbserver.srtm_dir, file));
+                xform = new gdal.CoordinateTransformation(gdal.SpatialReference.fromEPSG(4326), ds);
+                data = await ds.bands.get(1).pixels.readArrayAsync();
+            } else {
+                ds = last.ds;
+                xform = last.xform;
+                data = last.data;
             }
-        }
-        if (lastSRTMData) {
-            const pt = lastSRTMxform.transformPoint(p.lng, p.lat);
-            p.terrain = lastSRTMData.get(Math.round(pt.y), Math.round(pt.x));
+            const pt = xform.transformPoint(p.lng, p.lat);
+            p.terrain = data.get(Math.round(pt.y), Math.round(pt.x));
+
+            last.file = file;
+            last.ds = ds;
+            last.xform = xform;
+            last.data = data;
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error(e);
+            p.terrain = null;
         }
     }
 }
