b68807baa4bcc68a41571ea5476100cb6d8b8a50
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-09 16:14:50 +0200

    properly roll back transactions on error

--- src/back/import.ts
+++ src/back/import.ts
@@ -62,83 +62,93 @@ async function importFlight(file: string) {
         return;
     }
 
-    await db.query('BEGIN');
     const launch = score.opt['launch'];
     const launchFix = fixes[launch];
     const landing = score.opt['landing'];
+    const landingFix = fixes[landing];
 
-    r = await db.query(
-        'INSERT INTO flight (hash, launch_point, landing_point, ' +
-            ' p1_point, p2_point, p3_point, e1_point, e2_point, score, distance, category, type)' +
-            ' VALUES ( UNHEX(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )',
-        [
-            hash,
-            launch,
-            landing,
-            score.scoreInfo.tp[0].r,
-            score.scoreInfo.tp[1].r,
-            score.scoreInfo.tp[2].r,
-            score.scoreInfo.cp.in.r,
-            score.scoreInfo.cp.out.r,
-            score.score,
-            score.scoreInfo.distance,
-            desc.category,
-            desc.type.toUpperCase()
-        ]
-    );
-    const flightId = r['insertId'];
-    await db.query(
-        'INSERT INTO flight_extra (id, glider, pilot_url, flight_url, pilot_name, date, launch_lat, launch_lng,' +
-            ' p1_lat, p1_lng, p2_lat, p2_lng, p3_lat, p3_lng, e1_lat, e1_lng, e2_lat, e2_lng)' +
-            ' VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )',
-        [
-            flightId,
-            desc.glider,
-            desc.pilot_url,
-            desc.flight_url,
-            desc.pilot_name,
-            new Date(fixes[0].timestamp),
-            launchFix.latitude,
-            launchFix.longitude,
-            score.scoreInfo.tp[0].y,
-            score.scoreInfo.tp[0].x,
-            score.scoreInfo.tp[1].y,
-            score.scoreInfo.tp[1].x,
-            score.scoreInfo.tp[2].y,
-            score.scoreInfo.tp[2].x,
-            score.scoreInfo.cp.in.y,
-            score.scoreInfo.cp.in.x,
-            score.scoreInfo.cp.out.y,
-            score.scoreInfo.cp.out.x
-        ]
-    );
+    await db.query('BEGIN');
+    try {
+        r = await db.query(
+            'INSERT INTO flight (hash, launch_point, landing_point, ' +
+                ' p1_point, p2_point, p3_point, e1_point, e2_point, score, distance, category, type)' +
+                ' VALUES ( UNHEX(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )',
+            [
+                hash,
+                launch,
+                landing,
+                score.scoreInfo.tp[0].r,
+                score.scoreInfo.tp[1].r,
+                score.scoreInfo.tp[2].r,
+                score.scoreInfo.cp.in.r,
+                score.scoreInfo.cp.out.r,
+                score.score,
+                score.scoreInfo.distance,
+                desc.category,
+                desc.type.toUpperCase()
+            ]
+        );
+        const flightId = r['insertId'];
+        await db.query(
+            'INSERT INTO flight_extra (id, glider, pilot_url, flight_url, pilot_name, date,' +
+                ' launch_lat, launch_lng, landing_lat, landing_lng,' +
+                ' p1_lat, p1_lng, p2_lat, p2_lng, p3_lat, p3_lng, e1_lat, e1_lng, e2_lat, e2_lng)' +
+                ' VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )',
+            [
+                flightId,
+                desc.glider,
+                desc.pilot_url,
+                desc.flight_url,
+                desc.pilot_name,
+                new Date(fixes[0].timestamp),
+                launchFix.latitude,
+                launchFix.longitude,
+                landingFix.latitude,
+                landingFix.longitude,
+                score.scoreInfo.tp[0].y,
+                score.scoreInfo.tp[0].x,
+                score.scoreInfo.tp[1].y,
+                score.scoreInfo.tp[1].x,
+                score.scoreInfo.tp[2].y,
+                score.scoreInfo.tp[2].x,
+                score.scoreInfo.cp.in.y,
+                score.scoreInfo.cp.in.x,
+                score.scoreInfo.cp.out.y,
+                score.scoreInfo.cp.out.x
+            ]
+        );
 
-    const points = [];
-    for (let fixId = score.opt['launch']; fixId <= score.opt['landing']; fixId++) {
-        const fix = fixes[fixId];
-        if (!(fix.gpsAltitude || fix.pressureAltitude)) continue;
-        points.push([
-            fixId,
-            flightId,
-            fix.latitude,
-            fix.longitude,
-            fix.gpsAltitude || fix.pressureAltitude,
-            new Date(fix.timestamp)
-        ]);
+        const points = [];
+        for (let fixId = score.opt['launch']; fixId <= score.opt['landing']; fixId++) {
+            const fix = fixes[fixId];
+            if (!(fix.gpsAltitude || fix.pressureAltitude)) continue;
+            points.push([
+                fixId,
+                flightId,
+                fix.latitude,
+                fix.longitude,
+                fix.gpsAltitude || fix.pressureAltitude,
+                new Date(fix.timestamp)
+            ]);
+        }
+        await db.query('INSERT INTO point (id, flight_id, lat, lng, alt, time) VALUES ?', [points]);
+        await db.query('COMMIT');
+        console.log(`${file}: added ${points.length}/${fixes.length} points from flight ${flightId}`);
+    } catch (e) {
+        console.error(`${file}: transaction failed, rolling back: ${e}`);
+        await db.query('ROLLBACK');
     }
-    await db.query('INSERT INTO point (id, flight_id, lat, lng, alt, time) VALUES ?', [points]);
-    await db.query('COMMIT');
-    console.log(`${file}: added ${points.length}/${fixes.length} points from flight ${flightId}`);
 }
 
 async function main() {
     let arg = 2;
-    while (process.argv[arg])
+    while (process.argv[arg]) {
         try {
-            await importFlight(process.argv[arg++]);
+            await importFlight(process.argv[arg++]).catch((e) => console.error(process.argv[arg - 1], e));
         } catch (e) {
-            console.error(e);
+            console.error(process.argv[arg - 1], e);
         }
+    }
 }
 
 main().finally(() => db.close());
