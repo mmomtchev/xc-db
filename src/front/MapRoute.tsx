6cef2b33a094f90c6f94425887c19d146ec6b333
Momtchil Momtchev <momtchil@momtchev.com>
2023-06-11 15:55:08 +0200

    lint the project

--- src/front/MapRoute.tsx
+++ src/front/MapRoute.tsx
@@ -11,13 +11,12 @@ export default function MapRoute(props: {route: RouteInfo; highlight?: boolean})
     const pointRes = getPointResolution(context.map!.getView().getProjection(), 1, fromLonLat(props.route.tp[0]));
     const radius = Math.max(3000, props.route.avgDistance * 0.05 * 1000) / pointRes;
 
-    console.log(props);
     useEffect(() => {
         if (props.highlight) {
             const routeExtent = boundingExtent(props.route.tp.map((c) => fromLonLat(c)));
             context.map.getView().fit(routeExtent, {padding: [20, 20, 20, 20], duration: 750});
         }
-    }, [props.route.id, props.highlight]);
+    }, [props.route.tp, props.highlight, context.map]);
 
     return (
         <React.Fragment>
